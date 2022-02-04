cbbd9db15c6cb2793fea06982a9229b9f61af1ff
hectorNavarroMX <52169961+rtsistemaspuebla@users.noreply.github.com>
2022-02-04 00:04:11 -0600

    template header and footer added

--- index.php
+++ index.php
@@ -1,16 +1,13 @@
-<?php 
-echo "hola mundo";
-?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Sitio web</title>
-	<link rel="stylesheet" href="./css/bootstrap.min.css"/>
-</head>
-<body>
-	Sitio web
-</body>
-</html>
+<?php include('./template/cabecera.php'); ?>
+
+<div class="jumbotron">
+	<h1 class="display-3">Jumbo heading</h1>
+	<p class="lead">Jumbo helper text</p>
+	<hr class="my-2">
+	<p>More info</p>
+	<p class="lead">
+		<a class="btn btn-primary btn-lg" href="Jumbo action link" role="button">Jumbo action name</a>
+	</p>
+</div>
+
+<?php include('./template/pie.php'); ?>
